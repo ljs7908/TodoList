5b277e9f9bdf9b9ed29c5d4da74b296182f1f48f
jisu <lee@jisuui-MacBookPro.local>
2020-02-12 01:36:23 +0900

    TodoList Commit

---
+++ src/components/TodoHead.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+
+const TodoHeadBlock = styled.div`
+  padding-top: 48;
+  padding-right: 32px;
+  padding-left: 32px;
+  padding-bottom: 24px;
+  border-bottom: 1px solid #e0ecef;
+
+  hi {
+    margin: 0;
+    font-size: 36px;
+    color: #343a40;
+  }
+
+  .day {
+    margin-top: 4px;
+    color: #868e96;
+    font-size: 21px;
+  }
+
+  .tasks-left{
+      color : #20c997;
+      font-size: 18px;
+      margin-top: 40px
+      fornt-weight: bold;
+  }
+`;
+
+function TodoHead() {
+  return (
+    <TodoHeadBlock>
+      <h1>2020년 02월 09일</h1>
+
+      <div className="day"> 일요일 </div>
+      <div className="tasks-left"> 할 일 2개 남음</div>
+    </TodoHeadBlock>
+  );
+}
+
+export default TodoHead;
